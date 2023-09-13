b14304d777ed1e2e8cb280486010dd9fd883cf94
ing.dacastillop <ing.dacastillop@gmail.com>
2023-09-13 15:54:04 -0500

    :ambulance: Configure export folder types

--- rollup.config.js
+++ rollup.config.js
@@ -2,38 +2,63 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 
-export default {
-  input: 'dist/esm/index.js',
-  output: [
-    {
-      file: 'dist/es/lib.js',
-      format: 'es',
-      sourcemap: true,
-      inlineDynamicImports: true
-    },
-    {
-      file: 'dist/cjs/lib.cjs.js',
-      format: 'cjs',
-      sourcemap: true,
-      inlineDynamicImports: true
-    }
-  ],
-  external: [
-    '@rolster/typescript-invertly',
-    '@rolster/typescript-utils',
-    'dotenv',
-    'express',
-    'express-validator',
-    'reflect-metadata'
-  ],
-  plugins: [
-    resolve(),
-    commonjs(),
-    typescript({
-      tsconfig: './tsconfig.json',
-      declaration: true,
-      declarationDir: 'dist',
-      include: ['node_modules/@rolster/typescript-types/index.d.ts']
-    })
-  ]
-};
+const plugins = [
+  resolve(),
+  commonjs(),
+  typescript({
+    tsconfig: './tsconfig.json',
+    declaration: true,
+    declarationDir: 'dist',
+    include: ['node_modules/@rolster/typescript-types/index.d.ts']
+  })
+];
+
+const external = [
+  '@rolster/typescript-invertly',
+  '@rolster/typescript-utils',
+  'dotenv',
+  'express',
+  'express-validator',
+  'reflect-metadata'
+];
+
+export default [
+  {
+    input: 'dist/esm/index.js',
+    output: [
+      {
+        file: 'dist/es/index.js',
+        format: 'es',
+        sourcemap: true,
+        inlineDynamicImports: true
+      },
+      {
+        file: 'dist/cjs/index.js',
+        format: 'cjs',
+        sourcemap: true,
+        inlineDynamicImports: true
+      }
+    ],
+    external,
+    plugins
+  },
+  {
+    input: 'dist/esm/types/index.js',
+    output: [
+      {
+        file: 'dist/es/types/index.js',
+        format: 'es',
+        sourcemap: true,
+        inlineDynamicImports: true
+      },
+      {
+        file: 'dist/cjs/types/index.js',
+        format: 'cjs',
+        sourcemap: true,
+        inlineDynamicImports: true
+      }
+    ],
+    external,
+    plugins
+  }
+];

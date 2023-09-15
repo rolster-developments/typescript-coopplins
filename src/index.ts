1f32d07ed406b568197675d1804a77b607484c96
ing.dacastillop <ing.dacastillop@gmail.com>
2023-09-15 10:09:55 -0500

    :sparkles: Enabled Pubsub in root Project

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './decorators';
 export * from './exceptions';
+export * from './pubsub';
 export * from './results';
 export { coopplins, environment, validator } from './server';
 export { OnMiddleware, Result, ResultServer } from './types';
